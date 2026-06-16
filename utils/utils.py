c9907f1076507064a3bc93c1608d2d783605d174
neeraj214 <150280096+neeraj214@users.noreply.github.com>
2026-06-16 21:29:25 +0530

    Add utility functions for loss and metrics

---
+++ utils/utils.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+def dice_coef(y_true, y_pred, smooth=1e-6):
+    """
+    Dice Coefficient metric.
+    """
+    y_true_f = tf.keras.backend.flatten(tf.cast(y_true, tf.float32))
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred):
+    """
+    Dice Loss function.
+    """
+    return 1 - dice_coef(y_true, y_pred)
+
+def iou(y_true, y_pred, smooth=1e-6):
+    """
+    Intersection over Union metric.
+    """
+    y_true_f = tf.keras.backend.flatten(tf.cast(y_true, tf.float32))
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    union = tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) - intersection
+    return (intersection + smooth) / (union + smooth)
